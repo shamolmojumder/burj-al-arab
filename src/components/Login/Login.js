07da6ec417a3cfc5f85c81a3b7653ad722126b07
shamolmojumder <shamolmojumder14@gmail.com>
2021-05-31 21:10:19 +0600

    get simple google login auth set

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,42 @@
 import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { createGenerateClassName } from '@material-ui/styles';
+
 
 
 const Login = () => {
+    firebase.initializeApp(firebaseConfig);
+    const handleGoogleSignIn =()=>{
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          /** @type {firebase.auth.OAuthCredential} */
+          var credential = result.credential;
+      
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          console.log(user)
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
+    }
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn }>Google SignIn </button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyARRdJ9jsFQu_mOtvfgekeoyeI0rHGAA4Q",
+    authDomain: "burj-al-arab-18486.firebaseapp.com",
+    projectId: "burj-al-arab-18486",
+    storageBucket: "burj-al-arab-18486.appspot.com",
+    messagingSenderId: "971563680814",
+    appId: "1:971563680814:web:00cbacf160aa390a438bbf"
+  };
+  export default firebaseConfig;
