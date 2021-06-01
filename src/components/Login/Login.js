c05c576549268a02e256940afe0ab860c00acec5
shamolmojumder <shamolmojumder14@gmail.com>
2021-06-01 11:20:33 +0600

    create private route to protect a path

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,18 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
 import { createGenerateClassName } from '@material-ui/styles';
 import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
 
 
 
 const Login = () => {
   const [loggedInUser,setLoggedInUser]=useContext(UserContext);
+  const history=useHistory();
+  const location =useLocation();
+  const { from } = location.state || { from: { pathname: "/" } };
     if (firebase.apps.length===0) {
       firebase.initializeApp(firebaseConfig);    
     }
@@ -19,9 +23,10 @@ const Login = () => {
         .then((result) => {
           /** @type {firebase.auth.OAuthCredential} */
           var {displayName,email} = result.user;
-          const signInUser ={name:displayName,email}
-          console.log(result.user)
-          setLoggedInUser(signInUser)
+          const signInUser ={name:displayName,email};
+          console.log(result.user);
+          setLoggedInUser(signInUser);
+          history.replace(from);
           // ...
         }).catch((error) => {
           // Handle Errors here.
